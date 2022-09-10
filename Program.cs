0e981a2b3033e68c2962b3b1de42667f9a8e4427
Domen Koder <domen.koder@gmail.com>
2022-09-10 12:19:08 +0200

    Saving numbers in a list

--- Program.cs
+++ Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Input your binary number: ");
+
+            //Take the user input and save each number in a list
+            var input = Console.ReadLine();
+            var inputList = input.ToCharArray().ToList();
+
+            //Check the number of digits in the list and save it in an variable
+            var inputListCount = inputList.Count;
+
+            //Create a loop that uses inputListCount as the number for the itteration of loops
+            
+
+
         }
     }
 }
