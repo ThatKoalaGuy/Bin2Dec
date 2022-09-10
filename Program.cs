f72ed883b550d33f544d2192529505022fc7fc94
Domen Koder <domen.koder@gmail.com>
2022-09-10 12:31:44 +0200

    It's working

--- Program.cs
+++ Program.cs
@@ -13,10 +13,18 @@
             //Check the number of digits in the list and save it in an variable
             var inputListCount = inputList.Count;
 
-            //Create a loop that uses inputListCount as the number for the itteration of loops
-            
+            //Create a loop that loops as many times as the value of inputListCount
+            //and save the result of the calculation in a variable
+            var result = 0;
+            for (var i = 0; i < inputListCount; i++)
+            {
+                var binary = int.Parse(inputList[i].ToString());
+                var exponent = inputListCount - 1 - i;
+                result += binary * (int)Math.Pow(2, exponent);
+            }
 
-
+            //Print the result
+            Console.WriteLine(result);
         }
     }
 }
